1986c15a56874a3f9af76406486a3ff224a9723b
BohumelB <BohumelB@gmail.com>
2015-10-29 22:37:47 +0100

    Implementation of AdministratorDAO

--- travel-agency-persistence/src/main/java/cz/fi/muni/pa165/travelagency/dao/AdministratorDao.java
+++ travel-agency-persistence/src/main/java/cz/fi/muni/pa165/travelagency/dao/AdministratorDao.java
@@ -1,10 +1,26 @@
 package cz.fi.muni.pa165.travelagency.dao;
 
+import cz.fi.muni.pa165.travelagency.entity.Customer;
+import cz.fi.muni.pa165.travelagency.entity.Excursion;
+import cz.fi.muni.pa165.travelagency.entity.Reservation;
+import cz.fi.muni.pa165.travelagency.entity.Trip;
+import java.util.List;
+
 /**
  *
  * 
- * @author 
+ * @author Branislav Bohumel
  */
 public interface AdministratorDao {
+    public List<Customer> listCustomers();
+    public List<Reservation> listReservations();
+    public void createTrip(Trip trip);
+    public Trip readTrip(Long id);
+    public void updateTrip(Trip trip);
+    public void deleteTrip(Trip trip);
+    public void createExcursion(Excursion excursion);
+    public Excursion readExcursion(Long id);
+    public void updateExcursion(Excursion excursion); 
+    public void deleteExcursion(Excursion excursion);
     
 }
