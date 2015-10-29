42a283c998824d8809b048d0e79588985e96dd76
BohumelB <BohumelB@gmail.com>
2015-10-29 22:37:47 +0100

    Implementation of AdministratorDAO

--- travel-agency-persistence/src/main/java/cz/fi/muni/pa165/travelagency/dao/AdministratorDao.java
+++ travel-agency-persistence/src/main/java/cz/fi/muni/pa165/travelagency/dao/AdministratorDao.java
@@ -1,10 +1,26 @@
 package cz.fi.muni.pa165.travelagency.dao;
 
+import cz.fi.muni.pa165.travelagency.entity.Customer;
+import cz.fi.muni.pa165.travelagency.entity.Excursion;
+import cz.fi.muni.pa165.travelagency.entity.Reservation;
+import cz.fi.muni.pa165.travelagency.entity.Trip;
+import java.util.List;
+
 /**
  *
  * 
- * @author 
+ * @author Branislav Bohumel
  */
 public interface AdministratorDao {
+    public List<Customer> listCustomers();
+    public List<Reservation> listReservations();
+    public void createTrip(Trip trip);
+    public Trip readTrip(Long id);
+    public void updateTrip(Long oldTripId, Trip newTrip);
+    public void deleteTrip(Trip trip);
+    public void createExcursion(Excursion excursion);
+    public Excursion readExcursion(Long id);
+    public void updateExcursion(Long oldExcursionId, Excursion newExcursion); 
+    public void deleteExcursion(Excursion excursion);
     
 }
--- travel-agency-persistence/src/main/java/cz/fi/muni/pa165/travelagency/dao/AdministratorDaoImpl.java
+++ travel-agency-persistence/src/main/java/cz/fi/muni/pa165/travelagency/dao/AdministratorDaoImpl.java
@@ -1,10 +1,83 @@
 package cz.fi.muni.pa165.travelagency.dao;
 
+import cz.fi.muni.pa165.travelagency.entity.Trip;
+import cz.fi.muni.pa165.travelagency.entity.Excursion;
+import cz.fi.muni.pa165.travelagency.entity.Customer;
+import cz.fi.muni.pa165.travelagency.entity.Reservation;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
 /**
  *
  * 
- * @author 
+ * @author Branislav Bohumel
  */
 public class AdministratorDaoImpl extends UserDaoImpl implements AdministratorDao {
+    
+    @PersistenceContext
+    private EntityManager em;
+
+    //Functions returning lists of customers and reservations
+    @Override
+    public List<Customer> listCustomers() {
+	TypedQuery<Customer> query = em.createQuery("SELECT c FROM Customer c", Customer.class);
+	return query.getResultList();
+    }
+ 
+    @Override
+    public List<Reservation> listReservations() {
+	TypedQuery<Reservation> query = em.createQuery("SELECT r FROM Reservation r", Reservation.class);
+	return query.getResultList();
+    }
+    
+    //Following are CRUD operations for Administrator concerning Trip class
+    @Override
+    public void createTrip(Trip trip) {
+        em.persist(trip);
+    }  
+    
+    @Override
+    public Trip readTrip(Long id) {
+        return em.find(Trip.class, id);
+    }  
+    
+    @Override
+    public void updateTrip(Long oldTripId, Trip newTrip) {
+        Trip oldTrip = em.find(Trip.class, oldTripId);
+        em.remove(oldTrip);
+        em.persist(newTrip);
+    } 
+    
+    @Override
+    public void deleteTrip(Trip trip) {
+        em.remove(trip);
+    }
+    
+    //Following are CRUD operations for Administrator concerning Excursion class    
+    @Override
+    public void createExcursion(Excursion excursion) {
+        em.persist(excursion);
+    }
+    
+    @Override
+    public Excursion readExcursion(Long id) {
+        return em.find(Excursion.class, id);
+    }  
+    
+    @Override
+    public void updateExcursion(Long oldExcursionId, Excursion newExcursion) {
+        Excursion oldExcursion = em.find(Excursion.class, oldExcursionId);
+        em.remove(oldExcursion);
+        em.persist(newExcursion);
+    } 
+    
+    @Override
+    public void deleteExcursion(Excursion excursion) {
+        em.remove(excursion);
+    }
     
 }
