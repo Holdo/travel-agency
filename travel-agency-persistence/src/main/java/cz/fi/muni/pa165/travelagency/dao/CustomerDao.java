8c9ac46e943f5bc3bc33ee8de7e0b6bd8e8ded9e
Holdo <holic.michal@gmail.com>
2015-10-30 21:55:55 +0100

    Added javaDoc to CustomerDao interface

--- travel-agency-persistence/src/main/java/cz/fi/muni/pa165/travelagency/dao/CustomerDao.java
+++ travel-agency-persistence/src/main/java/cz/fi/muni/pa165/travelagency/dao/CustomerDao.java
@@ -4,15 +4,46 @@ import cz.fi.muni.pa165.travelagency.entity.Customer;
 import java.util.List;
 
 /**
- *
+ * Interface describing a customer in the system.
  *
  * @author Michal Holic
  */
 public interface CustomerDao {
 
+    /*
+     * Creates a customer entity in the database.
+     * 
+     * @param customer to be created, must not be null
+     */
     void create(Customer customer);
+    
+    /*
+     * Updates a customer entity in the database.
+     * 
+     * @param customer to be updated, must not be null
+     */
     void update(Customer customer);
+    
+    /*
+     * Deletes a customer entity in the database.
+     * 
+     * @param customer to be deleted, must not be null and must be in managed state
+     */
     void delete(Customer customer);
+    
+    /*
+     * Finds a customer in the database by his id.
+     * 
+     * @param customer id, must not be null
+     * @return Customer object or null, if not found
+     */
     Customer findById(Long id);
+    
+    /*
+     * Lists all the customers in the database.
+     * 
+     * @return List of all the customers in the database
+     *         List is empty if the are no customers in the database
+     */
     List<Customer> findAll();
 }
