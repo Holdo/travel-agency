629bbe5566a70f9f8d0c1c5335cc69870a859c54
wexik <julo.wex@gmail.com>
2015-10-30 23:07:11 +0100

    Reservation ExcursionDao ExcursionDaoImpl TripDaoTest

--- travel-agency-persistence/src/main/java/cz/fi/muni/pa165/travelagency/dao/ExcursionDao.java
+++ travel-agency-persistence/src/main/java/cz/fi/muni/pa165/travelagency/dao/ExcursionDao.java
@@ -1,10 +1,54 @@
 package cz.fi.muni.pa165.travelagency.dao;
 
+import java.util.List;
+
+import cz.fi.muni.pa165.travelagency.entity.Excursion;
+
 /**
- * 
- *
- * @author 
- */
+* 
+*
+* @author Julius Stassik
+*/
 public interface ExcursionDao {
     
+	/*
+	 * Create a excursion entity in the database.
+	 *
+	 * @param excursion to be created, must not be null
+	 */
+	void create (Excursion excursion);
+	
+	/*
+	 * Remove a excursion entity from the database.
+	 *
+	 * @param excursion to be deleted, must not be null 
+	 *	and must be in managed state
+	 */
+	void delete (Excursion excursion);
+	
+	/*
+	 * Update a excursion entity in the database.
+	 *
+	 * @param excursion to be updated, must not be null 
+	 */
+	void update (Excursion excursion);
+		
+	/*
+	 * Find a excursion in the database by its id.
+	 *
+	 * @param excursion id, must not be null
+	 * @return Excursion object or null, if not found
+	 */
+	Excursion findById(Long id);
+		
+	/*
+	 * Lists all the excursions from the database.
+	 *
+	 * @return List of all the excursions from the database
+	 * List is empty if the are no excursions in the database
+	 */
+	List<Excursion> findAll();
+		
 }
+
+    
--- travel-agency-persistence/src/main/java/cz/fi/muni/pa165/travelagency/entity/Reservation.java
+++ travel-agency-persistence/src/main/java/cz/fi/muni/pa165/travelagency/entity/Reservation.java
@@ -1,26 +1,43 @@
 package cz.fi.muni.pa165.travelagency.entity;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotNull;
 
 /**
- *
+ * Reservation entity
  * 
- * @author 
+ *
+ * @author Julius Stassik
  */
 @Entity
 public class Reservation implements Serializable {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+
+    @NotNull
+    @DecimalMin(value = "0")
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    @ManyToOne(optional=false)
+    private Customer customer;
+
     @ManyToMany(fetch = FetchType.LAZY)
     private Set<Trip> trips = new HashSet<>();
 
@@ -31,5 +48,49 @@ public class Reservation implements Serializable {
     public void setId(Long id) {
         this.id = id;
     }
-    
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Set<Trip> getTrips() {
+        return Collections.unmodifiableSet(this.trips);
+    }
+
+    public void addTrip(Trip trip) {
+        this.trips.add(trip);
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 29 * hash + Objects.hashCode(this.price);
+        hash = 29 * hash + Objects.hashCode(this.customer);
+        hash = 29 * hash + Objects.hashCode(this.trips);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (!(obj instanceof Reservation)) return false;
+        final Reservation other = (Reservation) obj;
+        if (!Objects.equals(this.price, other.getPrice())) return false;
+        if (!Objects.equals(this.customer, other.getCustomer())) return false;
+        if (!Objects.equals(this.trips, other.getTrips())) return false;
+        return true;
+    }
 }
