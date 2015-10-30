a45a11441f4c5c9073a6587ad3b186a424248da8
Holdo <holic.michal@gmail.com>
2015-10-30 23:07:01 +0100

    Added ReservationDao and ReservationDaoImpl

--- travel-agency-persistence/src/main/java/cz/fi/muni/pa165/travelagency/dao/ReservationDao.java
+++ travel-agency-persistence/src/main/java/cz/fi/muni/pa165/travelagency/dao/ReservationDao.java
@@ -1,10 +1,49 @@
 package cz.fi.muni.pa165.travelagency.dao;
 
+import cz.fi.muni.pa165.travelagency.entity.Reservation;
+import java.util.List;
+
 /**
- *
+ * Interface describing a reservation in the system.
  * 
- * @author 
+ * @author Michal Holic
  */
 public interface ReservationDao {
     
+    /*
+     * Creates a reservation entity in the database.
+     * 
+     * @param reservation to be created, must not be null
+     */
+    void create(Reservation reservation);
+    
+    /*
+     * Updates a reservation entity in the database.
+     * 
+     * @param reservation to be updated, must not be null
+     */
+    void update(Reservation reservation);
+    
+    /*
+     * Deletes a reservation entity in the database.
+     * 
+     * @param reservation to be deleted, must not be null and must be in managed state
+     */
+    void delete(Reservation reservation);
+    
+    /*
+     * Finds a reservation in the database by it's id.
+     * 
+     * @param reservation id, must not be null
+     * @return Reservation object or null, if not found
+     */
+    Reservation findById(Long id);
+    
+    /*
+     * Lists all the reservations in the database.
+     * 
+     * @return List of all the reservations in the database
+     *         List is empty if the are no reservations in the database
+     */
+    List<Reservation> findAll();
 }
--- travel-agency-persistence/src/main/java/cz/fi/muni/pa165/travelagency/dao/ReservationDaoImpl.java
+++ travel-agency-persistence/src/main/java/cz/fi/muni/pa165/travelagency/dao/ReservationDaoImpl.java
@@ -1,13 +1,44 @@
 package cz.fi.muni.pa165.travelagency.dao;
 
+import cz.fi.muni.pa165.travelagency.entity.Reservation;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
 
 /**
  *
  * 
- * @author 
+ * @author Michal Holic
  */
 @Repository
 public class ReservationDaoImpl implements ReservationDao {
     
+    @PersistenceContext
+    private EntityManager em;
+
+    @Override
+    public void create(Reservation reservation) {
+        em.persist(reservation);
+    }
+    
+    @Override
+    public void update(Reservation reservation) {
+        em.merge(reservation);
+    }
+    
+    @Override
+    public void delete(Reservation reservation) {
+        em.remove(reservation);
+    }
+    
+    @Override
+    public Reservation findById(Long id) {
+        return em.find(Reservation.class, id);
+    }
+
+    @Override
+    public List<Reservation> findAll() {
+        return em.createQuery("SELECT r FROM Reservation r", Reservation.class).getResultList();
+    }
 }
