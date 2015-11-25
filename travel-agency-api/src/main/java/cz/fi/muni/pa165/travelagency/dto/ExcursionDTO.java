c47c356dab07c3d98f1ac6ccc1c33fdc4e7ed37b
wexik <julo.wex@gmail.com>
2015-11-25 19:51:15 +0100

    ExcursionFacade ExcursionFacadeImpl ExcursionDTO TripService TripServiceImpl -> without nontrivial method

--- travel-agency-api/src/main/java/cz/fi/muni/pa165/travelagency/dto/ExcursionDTO.java
+++ travel-agency-api/src/main/java/cz/fi/muni/pa165/travelagency/dto/ExcursionDTO.java
@@ -1,9 +1,103 @@
 package cz.fi.muni.pa165.travelagency.dto;
 
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.time.Duration;
+import java.util.Objects;
+
 /**
  *
- * @author 
+ * @author Julius Stassik
  */
 public class ExcursionDTO {
+    
+    private Long id;
+    private Date excursionStartDay;
+    private Duration duration;
+    private String destination;
+    private BigDecimal price;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getExcursionStartDay() {
+        return excursionStartDay;
+    }
+
+    public void setExcursionStartDay(Date excursionStartDay) {
+        this.excursionStartDay = excursionStartDay;
+    }
+
+    public Duration getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Duration duration) {
+        this.duration = duration;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 31;
+        hash = 97 * hash + id.hashCode();
+        hash = 97 * hash + Objects.hashCode(this.excursionStartDay);
+        hash = 97 * hash + Objects.hashCode(this.duration);
+        hash = 97 * hash + Objects.hashCode(this.destination);
+        hash = 97 * hash + Objects.hashCode(this.price);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final ExcursionDTO other = (ExcursionDTO) obj;
+        if (!Objects.equals(this.destination, other.destination)) {
+            return false;
+        }
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        if (!Objects.equals(this.excursionStartDay, other.excursionStartDay)) {
+            return false;
+        }
+        if (!Objects.equals(this.duration, other.duration)) {
+            return false;
+        }
+        if (!Objects.equals(this.price, other.price)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
     
 }
---
+++ travel-agency-api/src/main/java/cz/fi/muni/pa165/travelagency/facade/ExcursionFacade.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.fi.muni.pa165.travelagency.facade;
+
+import cz.fi.muni.pa165.travelagency.dto.ExcursionDTO;
+import java.util.List;
+
+/**
+ *
+ * @author Julius Stassik
+ */
+public interface ExcursionFacade {
+    
+    /**
+     * Create excursion
+     * 
+     * @param excursion - excursion which will be created 
+     */
+    public void createExcursion(ExcursionDTO excursion);
+    
+    /**
+     * Delete excursion specified by its id
+     * 
+     * @param id - id of deleted excursion
+     */
+    public void deleteExcursion(Long id);
+    
+    /**
+     * Update excursion
+     * 
+     * @param excursion - excursion which will be updated
+     */
+    public void updateExcursion(ExcursionDTO excursion);
+    
+    /**
+     * Find excursion by its id
+     * 
+     * @param id - id of excursion which will be found
+     * @return ExcursionDTO - desired excursion or null when there is no 
+     *                         excursion with specified id
+     */
+    public ExcursionDTO getExcursionById(Long id);
+    
+    /**
+     * find all excursions
+     * 
+     * @return list of all excursions
+     */
+    public List<ExcursionDTO> getAllExcursions();
+
+      
+}
