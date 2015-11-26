69e6d94ced4b0663f92201a42e06161bc7727174
Branislav <bohumelb@gmail.com>
2015-11-26 21:55:41 +0100

    Implementation of AdministratorService + ReservationDTO

--- travel-agency-service/src/main/java/cz/fi/muni/pa165/travelagency/service/AdministratorServiceImpl.java
+++ travel-agency-service/src/main/java/cz/fi/muni/pa165/travelagency/service/AdministratorServiceImpl.java
@@ -1,12 +1,43 @@
 package cz.fi.muni.pa165.travelagency.service;
 
+import cz.fi.muni.pa165.travelagency.dao.AdministratorDao;
+import cz.fi.muni.pa165.travelagency.entity.Administrator;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
  *
- * @author 
+ * @author Branislav Bohumel
  */
 @Service
 public class AdministratorServiceImpl implements AdministratorService {
     
+    @Autowired
+    private AdministratorDao administratorDao;
+    
+    @Override
+    public void create(Administrator trip) {
+        administratorDao.create(trip);
+    }
+
+    @Override
+    public void update(Administrator trip) {
+        administratorDao.update(trip);
+    }
+
+    @Override
+    public void delete(Administrator trip) {
+        administratorDao.delete(trip);
+    }
+
+    @Override
+    public Administrator findById(Long id) {
+        return administratorDao.findById(id);
+    }
+
+    @Override
+    public List<Administrator> listAll() {
+	return administratorDao.findAll();   
+    }
 }
