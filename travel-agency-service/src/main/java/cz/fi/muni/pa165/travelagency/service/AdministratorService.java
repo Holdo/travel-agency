461aa94d880fc9702802e9932b643f5e75e43dcc
Branislav <bohumelb@gmail.com>
2015-11-27 19:25:57 +0100

    Implementation of TripFacade, JavaDoc for AdministratorService

--- travel-agency-service/src/main/java/cz/fi/muni/pa165/travelagency/service/AdministratorService.java
+++ travel-agency-service/src/main/java/cz/fi/muni/pa165/travelagency/service/AdministratorService.java
@@ -10,10 +10,42 @@ import java.util.List;
  */
 public interface AdministratorService {
     
+    
+    /*
+     * Creates an administrator 
+     * 
+     * @param administrator to be created
+     */
     void create(Administrator administrator);
+    /*
+     * Deletes an administrator
+     * 
+     * @param administrator to be deleted
+     */
     void update(Administrator administrator);
+    
+    /*
+     * Updates an administrator
+     * 
+     * @param administrator to be updated
+     */
     void delete(Administrator administrator);
+    
+    /*
+     * Finds and returns an administrator based on recieved id.
+     *
+     * @param administrator to be found 
+     * @return Administrator object
+     *         null if not found
+     */
     Administrator findById(Long id);
+    
+    /*
+     * Returns all the administrators.
+     * 
+     * @return List of all the administrators
+     *         empty List if the are no administrators
+     */
     List<Administrator> listAll();
     
 }
